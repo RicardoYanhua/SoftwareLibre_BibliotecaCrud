66002eb3bbd675d50c12a7f6fff8e9434249ca0c
EnvYend Ricardo YanHua <ricardoyanhua@gmail.com>
2025-11-14 10:49:10 -0500

    add frontend and backend

---
+++ biblioteca-crud/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express= require('express');
+const cors = require('cors');
+
+
+const libroRoutes = require('./routes/libroRoutes');
+const categoriaRoutes = require('./routes/categoriaRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api/libros', libroRoutes);
+app.use('/api/categorias', categoriaRoutes);
+
+
+app.get('/', (req,res)=>{
+    res.json({
+        mensaje:"API BIBLIOTECA - CRUD"
+    })
+})
+ 
+app.listen(PORT,()=>{
+    console.log(`Servidor inicializado en el http://localhost:${PORT}`)
+})

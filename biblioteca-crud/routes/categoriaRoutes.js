56e703de38d23d7195563cee6e1d77344b8fdbdf
EnvYend Ricardo YanHua <ricardoyanhua@gmail.com>
2025-11-14 10:57:44 -0500

    delete

--- biblioteca-crud/routes/categoriaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {
-obtenerLibroPorIdCategoria
-} = require('../controllers/categoriaController');
-
-//RUTAS CRUD
-router.get('/:id',obtenerLibroPorIdCategoria);
-module.exports=router;
